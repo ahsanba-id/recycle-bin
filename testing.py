268fd2e65ef8dc00b64c468d8ab34d10d63c1ea3
ahsanba <114816438+ahsanba-id@users.noreply.github.com>
2023-08-20 19:15:12 +0700

    Create testing.py
    Just for testing

---
+++ testing.py
@@ -0,0 +1,23 @@
+import stable_diffusion
+import upscaler.esrgan
+
+def generate_image(model_path, prompt, use_gpu=True):
+  image = stable_diffusion.generate_image(model_path, prompt, use_gpu)
+  return image
+
+def upscale_image(image, factor=4):
+  upscaled_image = upscaler.esrgan.upscale(image, factor)
+  return upscaled_image
+
+def main():
+  model_path = "path/to/model.ckpt"
+
+  # Get the prompt from the user.
+  prompt = input("Enter a prompt: ")
+
+  image = generate_image(model_path, prompt, use_gpu=True)
+  upscaled_image = upscale_image(image, factor=4)
+  print(upscaled_image)
+
+if __name__ == "__main__":
+  main()
