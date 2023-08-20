92b114642b2047d43d70d8397cef77f21d10831c
ahsanba <114816438+ahsanba-id@users.noreply.github.com>
2023-08-20 19:48:01 +0700

    Update testing.py

--- testing.py
+++ testing.py
@@ -5,19 +5,28 @@ def generate_image(model_path, prompt, use_gpu=True):
   image = stable_diffusion.generate_image(model_path, prompt, use_gpu)
   return image
 
-def upscale_image(image, factor=4):
-  upscaled_image = upscaler.esrgan.upscale(image, factor)
+def upscale_image(image, factor=4,base_size=(512, 512)):
+  upscaled_image = upscaler.esrgan.upscale(image, factor, base_size)
   return upscaled_image
 
-def main():
-  model_path = "models/testing.ckpt"
+def save_image(image, filename, quality=98):
+  image.save(filename, quality=quality)
 
-  # Get the prompt from the user.
+def main():
+  model_path = "path/to/model.ckpt"
   prompt = input("Enter a prompt: ")
 
-  image = generate_image(model_path, prompt, use_gpu=True)
-  upscaled_image = upscale_image(image, factor=4)
-  print(upscaled_image)
+  # Split the prompt into a positive prompt and a negative prompt.
+  positive_prompt, negative_prompt = prompt.split("-")
+
+  # Generate the image.
+  image = generate_image(model_path, positive_prompt, use_gpu)
+
+  # Upscale the image.
+  upscaled_image = upscale_image(image, factor=4, base_size=(512, 512))
+
+  # Save the image.
+  save_image(upscaled_image, "upscaled_image.jpg", quality=98)
 
 if __name__ == "__main__":
   main()
