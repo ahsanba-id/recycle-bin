b16bb28be9d4d6dc3decaf5fa8008e4f6389bf7e
ahsanba <114816438+ahsanba-id@users.noreply.github.com>
2023-08-22 09:08:10 +0700

    Create homework.py

---
+++ homework.py
@@ -0,0 +1,24 @@
+import subprocess
+import threading
+import time
+import socket
+import urllib.request
+
+def iframe_thread(port):
+  while True:
+      time.sleep(0.5)
+      sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+      result = sock.connect_ex(('127.0.0.1', port))
+      if result == 0:
+        break
+      sock.close()
+  print("\nComplete.\n")
+
+  p = subprocess.Popen(["cloudflared", "tunnel", "--url", "http://127.0.0.1:{}".format(port)], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+  for line in p.stderr:
+    l = line.decode()
+    if "trycloudflare.com " in l:
+      print("URL:", l[l.find("http"):], end='')
+    #print(l, end='')
+
+threading.Thread(target=iframe_thread, daemon=True, args=(8188,)).start()
